01be0de4b9d918e7a9ddce40e53e06579b1b689c
Sairaj G. Adhav <sairaj10adhav@gmail.com>
2025-06-21 22:57:34 +0530

    Use of MSE for compatibility

--- PruneKit/dragger.py
+++ PruneKit/dragger.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.metrics import root_mean_squared_error, accuracy_score
+from sklearn.metrics import mean_squared_error, accuracy_score
 
 
 def LinPruneReg(dataframe, target_column_name):
@@ -24,7 +24,7 @@ def LinPruneReg(dataframe, target_column_name):
         base_model = LinearRegression()
         base_model.fit(X_train_temp, y_train)
         base_predictions = base_model.predict(X_test_temp)
-        baseline_score = root_mean_squared_error(y_test, base_predictions)
+        baseline_score = mean_squared_error(y_test, base_predictions)
 
         feature_to_be_removed = ""
         min_score = baseline_score
@@ -37,7 +37,7 @@ def LinPruneReg(dataframe, target_column_name):
             model_iter = LinearRegression()
             model_iter.fit(X_train_iter, y_train)
             predictions_iter = model_iter.predict(X_test_iter)
-            current_score = root_mean_squared_error(y_test, predictions_iter)
+            current_score = mean_squared_error(y_test, predictions_iter)
 
             if current_score < min_score:
                 min_score = current_score
